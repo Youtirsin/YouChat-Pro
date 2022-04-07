773e606f4f4eabd88f716aef14fea88cd7ca4135
Youtirsin <1711221318@qq.com>
2022-04-07 16:41:20 +0800

    remove trash

--- client/src/request/request.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "RequestBase.h"
-#include "RegistrationReq.h"
-#include "PrivateReq.h"
-#include "FetchListReq.h"
-#include "BroadcastReq.h"
--- client/src/response/response.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ResponseBase.h"
-#include "RegistrationRes.h"
-#include "PrivateRes.h"
-#include "BroadcastRes.h"
-#include "FetchListRes.h"
